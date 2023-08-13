e1d54a2a82124ae43f84251b67ef9411064d92f3
GuiTalon <Guizitto@outlook.com.br>
2023-08-13 14:48:37 -0300

    JAVASCRIPT
    Exercício de javascript 004, fizemos um contador com contagem crescente e regressiva, confesso que meu raciocínio lógico estava porco, então é bom botar a mão na massa e melhorar isso, haha.

--- exercicios/DESAFIOS/DESAFIO03/script.js
+++ exercicios/DESAFIOS/DESAFIO03/script.js
@@ -6,14 +6,27 @@ function contar() {
    
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = `Impossível contar!`
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c+= p) {
-            res.innerHTML += `${c}`
+        if (p <=0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
+        if (i < f) {
+            // CONTAGEM CRESCENTE
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // CONTAGEM REGRESSIVA
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
