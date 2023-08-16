28c76c1b7912483e3df9a747cd8419077dd614b7
GuiTalon <Guizitto@outlook.com.br>
2023-08-16 14:49:42 -0300

    FUNÇÕES EM JAVASCRIPT
    Funções de javascript, com um exercício de par ou ímpar para testar.

--- exercicios/EXERCÍCIOS-NODE/ex009.js
+++ exercicios/EXERCÍCIOS-NODE/ex009.js
@@ -1,7 +1,8 @@
 let valores = [8, 1, 5, 7, 3]
-
+valores.sort()
 console.log(valores[0])
 
-for(let pos=0; pos < valores.length; pos++) {
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-}
+
+
+for(let pos in valores) {
+    console.log(`A posição${pos} tem o valor ${valores[pos]}`) }
