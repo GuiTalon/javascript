7fffde3f264c93b31962679f96a394dded280a79
GuiTalon <Guizitto@outlook.com.br>
2023-08-31 18:49:50 -0300

    OPERADOR SPREAD - FINAL
    Terminamos de ver o operador spread, um operador bem massa, um pouco complexo para um iniciante eu diria, mas isso é questão de prática.

--- JAVASCRIPT - CFBCursos/Aula012/aula012.js
+++ JAVASCRIPT - CFBCursos/Aula012/aula012.js
@@ -1,4 +1,7 @@
-const jogador1= {nome:"Guilherme",energia:100,vidas:3}
-const jogador2= {nome:"Talon",energia:100,vidas:5}
-const jogador3={...jogador1,...jogador2}
+const objs1=document.getElementsByTagName("div")
+const objs2=[...document.getElementsByTagName("div")]
 
+objs2.
+
+console.log(objs1)
+console.log(objs2)
