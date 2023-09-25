2e3b15dc4506c5029b20034bfd54e23d484b6d83
GuiTalon <Guizitto@outlook.com.br>
2023-09-25 07:50:37 -0300

    [ MAP FINAL e OPERADOR THIS em FUNÇÕES]
    Aprendemos sobre o operador THIS em funções e terminamos o uso do map. Aulas 028-029 respectivamente.

--- JAVASCRIPT - CFBCursos/Aula028/aula028.js
+++ JAVASCRIPT - CFBCursos/Aula028/aula028.js
@@ -1,3 +1,4 @@
-const el=document.getElementsByTagName("div")
-const val=Array.prototype.map.call(el, ({innerHTML})=>innerHTML)
-console.log()
+const converterInt=(e)=>parseInt(e)
+const dobrar=(e)=>e*2
+let num=['1','2','3','4','5'].map(dobrar)
+console.log(converterInt)
