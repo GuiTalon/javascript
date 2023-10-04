724591b8b15f0a644309e9e6c7233cdb2399dd96
GuiTalon <Guizitto@outlook.com.br>
2023-10-04 11:59:44 -0300

    DESAFIO DE ADDEVENTLISTENER - 035
    Quebrei um pouco a cabeça, mas decidi que seria melhor fazer uma forma de voltar com outro button, ao invés de apenas o de ir.

--- JAVASCRIPT - CFBCursos/Aula035 - ADDEVENTLISTENER/Aula035.js
+++ JAVASCRIPT - CFBCursos/Aula035 - ADDEVENTLISTENER/Aula035.js
@@ -2,10 +2,29 @@ const caixa1=document.querySelector('#caixa1')
 const caixa2=document.querySelector('#caixa2')
 const btn=document.querySelector('#btn_copiar')
 const todosCursos=[...document.querySelectorAll(".curso")]
+const btn2 = document.querySelector("#btn_remover")
 
 todosCursos.map((el)=>{
     el.addEventListener("click",(evt)=>{
         const curso=evt.target
-        console.log(curso)
+        curso.classList.toggle('selecionado')
     })
 })
+
+btn.addEventListener("click",()=>{
+    const cursosSelecionados=[...document.querySelectorAll('.selecionado')]
+    cursosSelecionados.map((el)=>{
+        caixa2.appendChild(el)
+        el.classList.remove('selecionado')
+    })
+})
+
+btn2.addEventListener('click',()=>{
+    const cursosSelecionados=[...document.querySelectorAll('.selecionado')]
+    cursosSelecionados.map((el)=>{
+        caixa1.appendChild(el)
+    })
+})
+
+
+
