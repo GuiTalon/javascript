9e220d6e4a9aa8ccd30394a1fe5bd205b85558ad
GuiTalon <Guizitto@outlook.com.br>
2023-10-16 15:50:03 -0300

    AppendChild e RemoveChild - 040-041
    Basicamente, vimos sobre como adicionar elementos em uma div sem o HTML, e como remover os mesmos, em aulas diferentes, claro. Aula 040 foi a de adicionar, 041 de remover.

--- JAVASCRIPT - CFBCursos/Aula041/Aula041.js
+++ JAVASCRIPT - CFBCursos/Aula041/Aula041.js
@@ -8,6 +8,16 @@ cursos.map((el,chave)=>{
     novoElemento.setAttribute('id','c'+chave)
     novoElemento.setAttribute('class','curso c1')
     novoElemento.innerHTML=el
+
+    const btn_lixeira=document.createElement("img")
+    btn_lixeira.setAttribute('src', './enemy.png')
+    btn_lixeira.setAttribute('class', 'btn_lixeira')
+    btn_lixeira.addEventListener('click',(evt)=>{
+        caixa1.removeChild(evt.target.parentNode)
+    })
+
+
+    novoElemento.appendChild(btn_lixeira)
     caixa1.appendChild(novoElemento)
 })
 
